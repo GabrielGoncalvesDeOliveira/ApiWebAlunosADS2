e59ed03c6be66655e8100c3bf762bab10b2a6ed5
Gabriel Oliveira <gabrielgo15@gmail.com>
2022-11-04 20:54:52 -0300

    Commit inicial. Criacao da classe Aluno. Configuracoes de contexto e conexao com o banco de dados.

---
+++ Models/Aluno.cs
@@ -0,0 +1,11 @@
+﻿namespace APIADS2022.Models
+{
+	public class Aluno
+	{
+		public int id { get; set; }
+		public String nome { get; set; }
+		public String disciplina { get; set; }
+		public float nota1 { get; set; }
+		public float nota2 { get; set; }
+	}
+}
